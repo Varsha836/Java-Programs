9d48bc1094f93f35cf9c9913db056cd7f19357f9
Varsha836 <114997243+Varsha836@users.noreply.github.com>
2022-10-04 16:58:38 +0530

    Add files via upload

---
+++ Countingloop.java
@@ -0,0 +1,15 @@
+package lec01;
+
+public class Countingloop {
+
+	public static void main(String[] args) {
+		int count = 1;
+		while (count <= 10) {
+
+			System.out.println(count);
+			count = count + 1;
+		}
+
+	}
+
+}
