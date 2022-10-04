954bcbc3a0ba9c29f8321657eabe3a10cb53d8fa
Varsha836 <114997243+Varsha836@users.noreply.github.com>
2022-10-04 16:58:58 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,14 @@
+package lec01;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		int count = 1; // initialization
+		while (count <= 5) {// condition
+			System.out.println("Hi");// work
+			count = count + 1; // update
+		}
+	}
+
+}
