3aab69e7eaa8ce1a032256c1efd2cd620f5a8a89
Varsha836 <114997243+Varsha836@users.noreply.github.com>
2022-10-04 17:04:48 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,23 @@
+package patternassignment;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int row = 1;
+		int numst = 1;
+		while (row <= n) {
+
+			int cst = 0;
+			while (cst < numst) {
+				System.out.print("* ");
+				cst++;
+			}
+			numst++;
+			row++;
+			System.out.println();
+
+		}
+
+	}
+}
---
+++ Question4.java
@@ -0,0 +1,33 @@
+package patternassignment;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+    int n = 5;
+    int numsp = n-1;
+    int numst = 1;
+    int row = 1;
+    while(row<=n) {
+    	
+    	int csp = 0;
+    	while(csp<numsp) {
+    		System.out.print("  ");
+    		csp++;
+    		
+    	}
+    	int cst = 0;
+    	while(cst<numst) {
+    		System.out.print(" *");
+    		cst++;
+    		
+    	}
+    	row++;
+    	System.out.println();
+    	numst++;
+    	numsp--;
+    	
+    }
+		
+	}
+
+}
